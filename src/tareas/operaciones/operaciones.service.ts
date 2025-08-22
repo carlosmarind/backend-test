6c9d12a0abc80e6337a6269e26d1f22daaa6c74f
EBenavidesL <edgardo.benavides@bupa.cl>
2025-08-22 14:29:14 -0400

    correccion jenkins

--- src/tareas/operaciones/operaciones.service.ts
+++ src/tareas/operaciones/operaciones.service.ts
@@ -2,17 +2,18 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class OperacionesService {
-  operar(operacion: string = '', a: number, b: number) {
-    if (operacion === 'suma') {
-      return this.#suma(a, b);
-    } else if (operacion === 'resta') {
-      return this.#resta(a, b);
-    } else if (operacion === 'multiplicacion') {
-      return this.#multiplicacion(a, b);
-    } else if (operacion === 'division') {
-      return this.#division(a, b);
-    }
+operar(a: number, b: number, operacion: string = '') {
+  if (operacion === 'suma') {
+    return this.#suma(a, b);
+  } else if (operacion === 'resta') {
+    return this.#resta(a, b);
+  } else if (operacion === 'multiplicacion') {
+    return this.#multiplicacion(a, b);
+  } else if (operacion === 'division') {
+    return this.#division(a, b);
   }
+}
+
 
   #suma(a: number, b: number) {
     return a + b;
