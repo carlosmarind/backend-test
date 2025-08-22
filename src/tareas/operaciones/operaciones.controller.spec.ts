d800f00272dea89417cfdc63a1c782186cc3873b
GiovanniD1694 <giovanni.diaz@ingetic.cl>
2025-08-22 02:02:50 -0400

    fix en .ts

--- src/tareas/operaciones/operaciones.controller.spec.ts
+++ src/tareas/operaciones/operaciones.controller.spec.ts
@@ -1,44 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { OperacionesController } from './operaciones.controller';
-import { OperacionesService } from './operaciones.service';
-import { Response } from 'express';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { App } from 'supertest/types';
+import { AppModule } from '../../../src/app.module';
 
-describe('OperacionesController', () => {
-  let controller: OperacionesController;
-
-  const mockResponse = () => {
-  const res: Partial<Response> = {};
-  res.status = jest.fn().mockReturnValue(res);
-  res.json = jest.fn().mockReturnValue(res);
-  return res as Response;
-};
+describe('AppController (e2e)', () => {
+  let app: INestApplication<App>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [OperacionesController],
-      providers: [OperacionesService],
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
     }).compile();
 
-    controller = module.get<OperacionesController>(OperacionesController);
+    app = moduleFixture.createNestApplication();
+    await app.init();
   });
 
-  it('retorna 200 para operacion valida', () => {
-    const res = mockResponse();
-    controller.operar(res, 'suma', 2, 3);
-    expect(res.status).toHaveBeenCalledWith(200);
-    expect(res.json).toHaveBeenCalledWith({
-      resultado: 5,
-      mensaje: 'operacion exitosa',
-    });
-  });
-
-  it('retorna 502 para operacion invalida', () => {
-    const res = mockResponse();
-    controller.operar(res, 'potencia', 2, 3);
-    expect(res.status).toHaveBeenCalledWith(502);
-    expect(res.json).toHaveBeenCalledWith({
-      resultado: NaN,
-      mensaje: 'operacion no pudo ser calculada',
-    });
+  it('/operaciones (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/operaciones')
+      .query({ operacion: 'suma', a: 10, b: 30 })
+      .expect(200);
   });
 });
