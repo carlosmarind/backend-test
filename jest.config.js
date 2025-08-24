345aa146376a04472eb01c3a97b2b908dcd0d431
EBenavidesL <edgardo.benavides@bupa.cl>
2025-08-23 20:25:05 -0400

    correccion quality gate

--- jest.config.js
+++ jest.config.js
@@ -5,12 +5,12 @@ testTimeout: 60000,
   testEnvironment: 'node',
   testMatch: ['**/*.spec.ts'],
   collectCoverageFrom: [
-    'src/**/*.{ts,js}',
-    '!src/main.ts',
-    '!src/**/*.module.ts',
-    '!src/**/*.spec.ts',
-    '!src/config/configuration.ts',
-    '!src/coverage/**'
+"src/**/*.{ts,js}",
+  "!src/main.ts",            // Excluir main.ts
+  "!src/**/*.module.ts",
+  "!src/**/*.spec.ts",
+  "!src/config/configuration.ts",
+  "!src/coverage/**"
   ],
   coverageReporters: ['lcov', 'text'],
   transform: {
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 // src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
