5a7b96d82c31f89eea8820021f92f9bbb6c0bfe1
cristofer <cristofer.mondaca@alu.ucm.cl>
2025-08-25 21:24:15 -0400

    valores fijos para variables

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,11 +1,11 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
-  port: parseInt(process.env.PORT ?? '3000', 10),
-  username: process.env.USERNAME ?? '',
-  apikey: process.env.API_KEY ?? '',
+  port: 3000,
+  username: 'usuario',
+  apikey: 'API_KEY',  
   database: {
-    host: process.env.DATABASE_HOST ?? 'localhost',
-    port: parseInt(process.env.DATABASE_PORT ?? '5432', 10),
+    host: 'localhost',
+    port: 5432,
   },
 }));
