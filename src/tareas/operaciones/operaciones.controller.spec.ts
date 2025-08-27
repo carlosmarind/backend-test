766b37994202f958517981a167481fa4107c65af
catalinaIlabaca <catalina.a.ilabaca@uchile.cl>
2025-08-27 00:13:11 -0400

    test: add new test to operaciones

--- src/tareas/operaciones/operaciones.controller.spec.ts
+++ src/tareas/operaciones/operaciones.controller.spec.ts
@@ -15,11 +15,74 @@ describe('AppController (e2e)', () => {
     app = moduleFixture.createNestApplication();
     await app.init();
   });
-
-  it('/operaciones (GET)', () => {
+  it('/operaciones (GET) suma', () => {
     return request(app.getHttpServer())
       .get('/operaciones')
-      .query({ operacion: 'suma', a: 10, b: 30 })
-      .expect(200);
+      .query({ operacion: 'suma', a: 15, b: 10 })
+      .expect(200)
+      .expect({ resultado: 25, mensaje: 'operacion exitosa' }).then((res) => {
+        expect(res.body.resultado).toBe(25);
+        expect(res.body.mensaje).toBe('operacion exitosa');
+      });
+  });
+  it('/operaciones (GET) resta', () => {
+  return request(app.getHttpServer())
+    .get('/operaciones')
+    .query({ operacion: 'resta', a: 5, b: 30 })
+    .expect(200)
+    .expect({ resultado: -25, mensaje: 'operacion exitosa' }).then((res) => {
+      expect(res.body.resultado).toBe(-25);
+      expect(res.body.mensaje).toBe('operacion exitosa');
+    });
+  });
+  it('/operaciones (GET) multiplicacion', () => {
+  return request(app.getHttpServer())
+    .get('/operaciones')
+    .query({ operacion: 'multiplicacion', a: 5, b: 5 })
+    .expect(200)
+    .expect({ resultado: 25, mensaje: 'operacion exitosa' }).then((res) => {
+      expect(res.body.resultado).toBe(25);
+      expect(res.body.mensaje).toBe('operacion exitosa');
+    });
+  });
+  it('/operaciones (GET) division', () => {
+  return request(app.getHttpServer())
+    .get('/operaciones')
+    .query({ operacion: 'division', a: 250, b: 10 })
+    .expect(200)
+    .expect({ resultado: 25, mensaje: 'operacion exitosa' }).then((res) => {
+      expect(res.body.resultado).toBe(25);
+      expect(res.body.mensaje).toBe('operacion exitosa');
+    });
+  });
+  it('/operaciones (GET) division con cero en dividendo', () => {
+  return request(app.getHttpServer())
+    .get('/operaciones')
+    .query({ operacion: 'division', a: 0, b: 25 })
+    .expect(502)
+    .expect({ resultado: null, mensaje: 'operacion no pudo ser calculada' }).then((res) => {
+      expect(res.body.resultado).toBe(null);
+      expect(res.body.mensaje).toBe('operacion no pudo ser calculada');
+    });
+  });
+  it('/operaciones (GET) division con cero en divisor', () => {
+  return request(app.getHttpServer())
+    .get('/operaciones')
+    .query({ operacion: 'division', a: 25, b: 0 })
+    .expect(502)
+    .expect({ resultado: null, mensaje: 'operacion no pudo ser calculada' }).then((res) => {
+      expect(res.body.resultado).toBe(null);
+      expect(res.body.mensaje).toBe('operacion no pudo ser calculada');
+    });
+  });
+   it('/operaciones (GET) numeros no validos', () => {
+    return request(app.getHttpServer())
+      .get('/operaciones')
+      .query({ operacion: 'resta', a: 'numero', b: 'otro  numero' })
+      .expect(502)
+      .expect({ resultado: null, mensaje: 'operacion no pudo ser calculada'}).then((res) => {
+        expect(res.body.resultado).toBe(null);
+        expect(res.body.mensaje).toBe('operacion no pudo ser calculada');
+      });
   });
 });
